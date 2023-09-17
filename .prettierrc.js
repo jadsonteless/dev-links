7f0b18d0a63be4244d54c0c8b1001a7b1fb5e591
jadsonteless <jtcostha@gmail.com>
2023-09-17 12:36:42 -0300

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // Use 2 espaços para a formatação do código
+  tabWidth: 2,
+  // Use aspas simples para strings
+  singleQuote: true,
+  // Ponto e vírgula no final de cada instrução
+  semi: true,
+  // Quebra de linha no final do arquivo
+  trailingComma: "all",
+  // Quebra de linha de acordo com o sistema operacional
+  endOfLine: "auto",
+};
