ffede9da514a480fce07264777a3811c47e8fdbf
jadsonteless <jtcostha@gmail.com>
2023-09-20 00:11:52 -0300

    black e light finish

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMode() {
+  const body = document.body;
+  body.classList.toggle('light');
+
+  // if(body.classList.contains('light')) {
+  //   body.classList.remove('light');
+  // } else {
+  //   body.classList.add('light');
+  // }
+
+  const img = document.querySelector('#profile img');
+  if (body.classList.contains('light')) {
+    img.setAttribute('src', './assets/avatar-light.png');
+    img.setAttribute(
+      'alt',
+      'foto de jadson sorrindo, usando óculos escuros, com o fundo branco',
+      );
+    } else {
+    img.setAttribute('src', './assets/avatar.png');
+    img.setAttribute(
+      'alt',
+      'foto de jadson sorrindo, usando roupa social com fundo preto',
+    );
+  }
+  console.log(img.alt)
+}
