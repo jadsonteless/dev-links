e6e73641d4ddd111508c866c9f0ec216731e759c
jadsonteless <jtcostha@gmail.com>
2023-09-21 12:23:43 -0300

    add do if para carrega foto do localstorage

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const black = document.querySelector('#black');
 const img = document.querySelector('#profile img');
-// confirm.log(img);
-// localStorage.clear()
+
 document.addEventListener('DOMContentLoaded', function () {
   const tema = localStorage.getItem('tema');
   const foto = localStorage.getItem('foto')
   console.log(foto)
   black.classList.add(tema);
-  img.setAttribute('src', foto);
+  if(foto) {
+    img.setAttribute('src', foto);
+  }
 });
 
 function toggleMode() {
@@ -20,14 +21,12 @@ function trocarTema() {
     black.classList.add('black');
     localStorage.setItem('tema', 'black');
     localStorage.setItem('foto', './assets/avatar.png');
-    console.log('trocou para black');
     img.setAttribute('src', './assets/avatar.png');
   } else {
     black.classList.remove('black');
     black.classList.add('light');
     localStorage.setItem('tema', 'light');
     localStorage.setItem('foto', './assets/avatar-light.png');
-    console.log('trocou para light');
     img.setAttribute('src', './assets/avatar-light.png');
   }
 }
