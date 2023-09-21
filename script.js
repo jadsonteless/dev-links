804c14a11ad37c493241ee24572d5bd65c6c0105
jadsonteless <jtcostha@gmail.com>
2023-09-21 12:00:51 -0300

    correção na troca de foto do perfil

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-const black = document.querySelector('.black')
+const black = document.querySelector('#black');
 const img = document.querySelector('#profile img');
-
+// confirm.log(img);
+// localStorage.clear()
 document.addEventListener('DOMContentLoaded', function () {
-  const tema = localStorage.getItem('tema')
-  black.classList.add(tema)
-  console.log(img)
-})
+  const tema = localStorage.getItem('tema');
+  const foto = localStorage.getItem('foto')
+  console.log(foto)
+  black.classList.add(tema);
+  img.setAttribute('src', foto);
+});
 
 function toggleMode() {
-  
-  if(black.classList.value == 'light') {
-    black.classList.remove('light')
-    black.classList.add('black')
-    localStorage.setItem('tema', 'black')
+  trocarTema()
+}
+
+function trocarTema() {
+  if (black.classList.value == 'light') {
+    black.classList.remove('light');
+    black.classList.add('black');
+    localStorage.setItem('tema', 'black');
+    localStorage.setItem('foto', './assets/avatar.png');
+    console.log('trocou para black');
     img.setAttribute('src', './assets/avatar.png');
-    img.setAttribute(
-      'alt',
-      'foto de jadson sorrindo, usando roupa social com fundo preto',
-    );
   } else {
-    black.classList.remove('black')
-    black.classList.add('light')
-    localStorage.setItem('tema', 'light')
+    black.classList.remove('black');
+    black.classList.add('light');
+    localStorage.setItem('tema', 'light');
+    localStorage.setItem('foto', './assets/avatar-light.png');
+    console.log('trocou para light');
     img.setAttribute('src', './assets/avatar-light.png');
-    img.setAttribute(
-      'alt',
-      'foto de jadson sorrindo, usando óculos escuros, com o fundo branco',
-    );
   }
-  console.log(black.classList.value)
 }
