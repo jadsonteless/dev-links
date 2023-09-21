e42e75bc808389c9192a51c240f2e6767a24e611
jadsonteless <jtcostha@gmail.com>
2023-09-21 02:58:39 -0300

    salvando preferencia do tema com  localstorage

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+
+
+const black = document.querySelector('.black')
+document.addEventListener('DOMContentLoaded', function () {
+  let tema = localStorage.getItem('tema')
+  black.classList.add(tema)
+});
 function toggleMode() {
-  const body = document.body;
-  body.classList.toggle('light');
+  console.log(black.classList.value)
+  
+  if(black.classList.value == 'light') {
+    black.classList.remove('light')
+    black.classList.add('black')
+    localStorage.setItem('tema', 'black')
+  } else {
+    black.classList.remove('black')
+    black.classList.add('light')
+    localStorage.setItem('tema', 'light')
+  }
+  
+  console.log(black.classList.value)
+  // const body = document.body;
+  // // body.classList.toggle('light');
+
+  // if(body.classList.contains('light')) {
+  //   body.classList.remove('light');
+  // } else {
+  //   localStorage.setItem('theme', 'light');
+  //   const cor2 = localStorage.getItem('theme')
+  //   console.log(typeof cor2)
+  //   body.classList.add(cor2);
+  //   console.log(body.classList)
+  // }
 
   // if(body.classList.contains('light')) {
   //   body.classList.remove('light');
@@ -8,19 +38,20 @@ function toggleMode() {
   //   body.classList.add('light');
   // }
 
-  const img = document.querySelector('#profile img');
-  if (body.classList.contains('light')) {
-    img.setAttribute('src', './assets/avatar-light.png');
-    img.setAttribute(
-      'alt',
-      'foto de jadson sorrindo, usando óculos escuros, com o fundo branco',
-      );
-    } else {
-    img.setAttribute('src', './assets/avatar.png');
-    img.setAttribute(
-      'alt',
-      'foto de jadson sorrindo, usando roupa social com fundo preto',
-    );
-  }
+  // localStorage.clear();
+  // const img = document.querySelector('#profile img');
+  // if (body.classList.contains('light')) {
+  //   img.setAttribute('src', './assets/avatar-light.png');
+  //   img.setAttribute(
+  //     'alt',
+  //     'foto de jadson sorrindo, usando óculos escuros, com o fundo branco',
+  //   );
+  // } else {
+  //   img.setAttribute('src', './assets/avatar.png');
+  //   img.setAttribute(
+  //     'alt',
+  //     'foto de jadson sorrindo, usando roupa social com fundo preto',
+  //   );
+  // }
   // console.log(img.alt)
 }
